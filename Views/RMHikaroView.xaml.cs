086438934daca37e93ad7ff840cf254a87482084
hikaroaugusto <hikaro.oliveira@usp.br>
2018-05-28 12:03:15 -0300

    Add files via upload

---
+++ Views/RMHikaroView.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: hikar
+ * Date: 05/27/2018
+ * Time: 16:47
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace RoadMapWPF.Views
+{
+	/// <summary>
+	/// Interaction logic for RMHikaroView.xaml
+	/// </summary>
+	public partial class RMHikaroView : UserControl
+	{
+		public RMHikaroView()
+		{
+			InitializeComponent();
+		}
+	}
+}
