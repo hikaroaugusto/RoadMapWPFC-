9547ab932e44d13c7e8f9038177f92f2913020cb
hikaroaugusto <hikaro.oliveira@usp.br>
2018-05-28 12:03:30 -0300

    Add files via upload

---
+++ ViewModels/RMGeralViewModel.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: hikar
+ * Date: 05/27/2018
+ * Time: 16:49
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace RoadMapWPF.ViewModels
+{
+	/// <summary>
+	/// Description of RMGeralViewModel.
+	/// </summary>
+	public class RMGeralViewModel
+	{
+		public RMGeralViewModel()
+		{
+		}
+	}
+}
---
+++ ViewModels/RMHikaroViewModel.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: hikar
+ * Date: 05/27/2018
+ * Time: 16:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace RoadMapWPF.ViewModels
+{
+	/// <summary>
+	/// Description of RMHikaroViewModel.
+	/// </summary>
+	public class RMHikaroViewModel
+	{
+		public RMHikaroViewModel()
+		{
+		}
+	}
+}
